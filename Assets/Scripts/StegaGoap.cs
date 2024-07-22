1e73cffcc53c77ecbcdc39540a21131327a53bac
Josh <1029946@student.sae.edu.au>
2024-07-22 16:50:43 +1000

    start of goap

---
+++ Assets/Scripts/StegaGoap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StegaGoap : MonoBehaviour
+{
+    public bool hungry = false;
+    public bool thirsty = false;
+    public bool scared = false;
+    public bool tired = false; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -11,17 +11,24 @@ public class TerrainGeneration : MonoBehaviour
         // Get the terrain data
         TerrainData terrainData = terrain.terrainData;
 
-        // Generate random heights for the terrain
+        // Generate random heights for the terrain with fall-off effect
         float[,] heights = new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
+        float center = terrainData.heightmapResolution / 2f; // Calculate the center of the terrain
+
         for (int i = 0; i < terrainData.heightmapResolution; i++)
         {
             for (int j = 0; j < terrainData.heightmapResolution; j++)
             {
-                heights[i, j] = Random.Range(0.0f, 0.5f)/100; // Random height value between 0 and 1
+                float distanceFromCenter = Vector2.Distance(new Vector2(i, j), new Vector2(center, center));
+                float normalizedDistance = distanceFromCenter / center; // Normalize the distance
+
+                // Apply fall-off effect to the height based on the normalized distance
+                float fallOff = Mathf.Clamp01(1f - normalizedDistance);
+                heights[i, j] = Random.Range(0.0f, 0.05f) * fallOff; // Apply fall-off to the random height
             }
         }
 
-        // Apply the generated heights to the terrain
+        // Apply the generated heights with fall-off effect to the terrain
         terrainData.SetHeights(0, 0, heights);
     }
 
