1217d57df0344b1e7ea04c9416b10a36804a8a05
Josh <1029946@student.sae.edu.au>
2024-07-25 16:59:33 +1000

    basic goap working (9)
    finsihed part 9 and got dino moveing

--- Assets/Scripts/GAction.cs
+++ Assets/Scripts/GAction.cs
@@ -2,16 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public abstract class GAction : MonoBehaviour
 {
     public string actionName = "Action";
     public float cost = 1.0f;
     public GameObject target;
-    public GameObject targetTag;
+    public string targetTag;
     public float duration = 0;
     public WorldState[] preConditions;
     public WorldState[] afterEffects;
+    public NavMeshAgent agent;
 
     public Dictionary<string, int> preconditions;
     public Dictionary<string, int> effects;
@@ -28,6 +30,7 @@ public abstract class GAction : MonoBehaviour
 
     public void Awake()
     {
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
         if (preConditions != null)
         {
             foreach (WorldState w in preConditions)
